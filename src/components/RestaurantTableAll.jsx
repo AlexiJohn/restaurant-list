dcd094a4bf6af7a8f96da53d9ffb02c6335f514d
AlexiJohn <56110779+AlexiJohn@users.noreply.github.com>
2023-11-28 05:25:45 +0800

    Removed soft patch for search, moving to Reducers

--- src/components/RestaurantTableAll.jsx
+++ src/components/RestaurantTableAll.jsx
@@ -3,19 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function RestaurantTable() {
-    const { searchedRestaurants, restaurants } = useSelector((state) => state.restaurant);
+    const { searchedRestaurants } = useSelector((state) => state.restaurant);
 
     const restaurantsByState = {};
-
-    if (searchedRestaurants == []){
-        restaurants.forEach(({ state, restaurant_name }) => {
-    if (!restaurantsByState[state]) {
-      restaurantsByState[state] = [];
-    }
-    restaurantsByState[state].push(restaurant_name);
-    restaurantsByState[state].sort();
-  });
-    } else if {
     
     searchedRestaurants.forEach(({ state, restaurant_name }) => {
     if (!restaurantsByState[state]) {
@@ -24,7 +14,7 @@ function RestaurantTable() {
     restaurantsByState[state].push(restaurant_name);
     restaurantsByState[state].sort();
   });
-    }
+    
   return (
     <div className="container mt-4">
       <table className="table">
