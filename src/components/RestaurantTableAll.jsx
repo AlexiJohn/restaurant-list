d6d2840cbc12129d42bb9c73cdd97f0f020eec47
AlexiJohn <56110779+AlexiJohn@users.noreply.github.com>
2023-11-28 05:20:28 +0800

    Update RestaurantTableAll to update on start

--- src/components/RestaurantTableAll.jsx
+++ src/components/RestaurantTableAll.jsx
@@ -3,10 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function RestaurantTable() {
-    const { searchedRestaurants } = useSelector((state) => state.restaurant);
+    const { searchedRestaurants, restaurants } = useSelector((state) => state.restaurant);
 
     const restaurantsByState = {};
 
+    if (searchedRestaurants == []){
+        restaurants.forEach(({ state, restaurant_name }) => {
+    if (!restaurantsByState[state]) {
+      restaurantsByState[state] = [];
+    }
+    restaurantsByState[state].push(restaurant_name);
+    restaurantsByState[state].sort();
+  });
+    } else if {
+    
     searchedRestaurants.forEach(({ state, restaurant_name }) => {
     if (!restaurantsByState[state]) {
       restaurantsByState[state] = [];
@@ -14,7 +24,7 @@ function RestaurantTable() {
     restaurantsByState[state].push(restaurant_name);
     restaurantsByState[state].sort();
   });
-
+    }
   return (
     <div className="container mt-4">
       <table className="table">
